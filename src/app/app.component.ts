d1bd58b4ae2403afc98b577ab0cb674ab92125cd
realabir <sikder.abir@gmail.com>
2023-06-02 17:46:58 +0200

    runder radierer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -104,20 +104,19 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   erase(x: number, y: number) {
-    const radius = this.eraserSize / 2;
+    const maxDistance = this.eraserSize / 2;
     const imageData = this.context.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
     const data = imageData.data;
     const width = this.canvas.nativeElement.width;
     const height = this.canvas.nativeElement.height;
 
-    const stepSize = 0.5; // Kleinere Schrittweite für dichtere Überprüfung
-
-    for (let pixelY = 0; pixelY < height; pixelY += stepSize) {
-      for (let pixelX = 0; pixelX < width; pixelX += stepSize) {
+    for (let pixelY = 0; pixelY < height; pixelY++) {
+      for (let pixelX = 0; pixelX < width; pixelX++) {
         const pixelIndex = (pixelY * width + pixelX) * 4;
         const distance = Math.hypot(pixelX - x, pixelY - y);
-        if (distance <= radius) {
-          data[pixelIndex + 3] = 0;
+        const opacity = 1 - distance / maxDistance; // Berechne die Transparenz basierend auf der Entfernung zum Radierpunkt
+        if (opacity > 0) {
+          data[pixelIndex + 3] *= opacity; // Multipliziere den aktuellen Alpha-Kanal mit der Transparenz
         }
       }
     }
